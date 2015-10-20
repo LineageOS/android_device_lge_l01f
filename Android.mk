0ca76777a9b4c0ce94f4d3984a97dd647da9449f
supersonicninjaxda <xdaninja@yahoo.co.jp>
2015-10-20 17:27:51 +0900

    Add new device:l01f
    L01F is a Japanese variant of the LG G2.
    The commit is based on android_device_lge_d802 which is basically the same phone.
    NFC is disabled since l01f uses sony cxd2235 nfc chip which is different from other variant and has no open-source support yet.
    
    Change-Id: I3dbf9ac9ddbc249b83d6fdf4c7694cf08edb4a86

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
-ifneq ($(filter l01f,$(TARGET_DEVICE)),)
+ifneq ($(filter d802,$(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo cm_l01f-eng
+add_lunch_combo cm_l01f-userdebug
